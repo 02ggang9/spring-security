485bd34a7792c788f088f0d1f91a60240b8971d5
02ggang9 <02ggang9@gmail.com>
2023-08-31 14:48:53 +0900

    feat: 1-2 사용자 정의 보안 기능 구현

---
+++ src/main/java/com/example/security/controller/SecurityController.java
@@ -0,0 +1,17 @@
+package com.example.security.controller;
+
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Validated
+@RestController
+public class SecurityController {
+
+  @GetMapping("/")
+  public String index() {
+    return "home";
+  }
+
+}
