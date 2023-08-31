c4369aab6d63535e64d41b72cb3d52781f9392c4
02ggang9 <02ggang9@gmail.com>
2023-08-31 15:03:22 +0900

    formLogin Using Lamda

--- src/main/java/com/example/security/config/SecurityConfig.java
+++ src/main/java/com/example/security/config/SecurityConfig.java
@@ -2,12 +2,20 @@ package com.example.security.config;
 
 import static org.springframework.security.config.Customizer.*;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -16,8 +24,30 @@ public class SecurityConfig {
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http.authorizeHttpRequests((authorize) -> authorize
-        .anyRequest().authenticated()
-    ).formLogin(withDefaults());
+            .anyRequest().authenticated()
+        )
+        .formLogin((formLogin) -> formLogin
+            .loginPage("/loginPage")
+            .defaultSuccessUrl("/")
+            .failureUrl("/login")
+            .usernameParameter("userId")
+            .passwordParameter("password")
+            .loginProcessingUrl("/login_proc")
+            .successHandler(new AuthenticationSuccessHandler() {
+              @Override
+              public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                  Authentication authentication) throws IOException, ServletException {
+                System.out.println("name -> " + authentication.getName());
+              }
+            })
+            .failureHandler(new AuthenticationFailureHandler() {
+              @Override
+              public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+                  AuthenticationException exception) throws IOException, ServletException {
+                System.out.println("exception -> " + exception.getMessage());
+              }
+            })
+            .permitAll());
 
     return http.build();
 
--- src/main/java/com/example/security/controller/SecurityController.java
+++ src/main/java/com/example/security/controller/SecurityController.java
@@ -14,4 +14,9 @@ public class SecurityController {
     return "home";
   }
 
+  @GetMapping("/loginPage")
+  public String loginPage() {
+    return "loginPage";
+  }
+
 }
